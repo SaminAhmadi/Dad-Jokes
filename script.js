b65c818c344d95c2bcef29da96bd6ad956684fda
Samin Ahmadi <Saminspersonal@gmail.com>
2023-11-01 13:52:43 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const jokeElement = document.querySelector(".joke");
+const jokeBtn = document.querySelector(".jokeBtn");
+
+jokeBtn.addEventListener("click", generateJoke);
+
+generateJoke();
+
+function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+  fetch("https://icanhazdadjoke.com", config)
+    .then((res) => res.json())
+    .then((data) => {
+      jokeElement.innerHTML = `${data.joke}`;
+    });
+}
